487c0b84325982fa504dd574bb2a189ee5a4b44f
EduarteXD <i@m.oxdl.cn>
2024-05-13 16:32:54 +0800

    feat: 🎸 layer-diffuse

---
+++ predict.py
@@ -0,0 +1,196 @@
+from cog import BasePredictor, Input, Path # type: ignore
+from typing import List
+import os
+import random
+from typing import Sequence, Mapping, Any, Union
+import torch
+import utils.samplers
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+
+def get_value_at_index(obj: Union[Sequence, Mapping], index: int) -> Any:
+    try:
+        return obj[index]
+    except KeyError:
+        return obj["result"][index]
+
+
+def find_path(name: str, path: str = None) -> str:
+    if path is None:
+        path = os.getcwd()
+
+    if name in os.listdir(path):
+        path_name = os.path.join(path, name)
+        print(f"{name} found: {path_name}")
+        return path_name
+
+    parent_directory = os.path.dirname(path)
+
+    if parent_directory == path:
+        return None
+
+    return find_path(name, parent_directory)
+
+'''
+def add_comfyui_directory_to_sys_path() -> None:
+    comfyui_path = find_path("ComfyUI")
+    if comfyui_path is not None and os.path.isdir(comfyui_path):
+        sys.path.append(comfyui_path)
+        print(f"'{comfyui_path}' added to sys.path")
+
+
+add_comfyui_directory_to_sys_path()
+
+
+def import_custom_nodes() -> None:
+    import asyncio
+    import execution
+    from nodes import init_custom_nodes
+    import server
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    server_instance = server.PromptServer(loop)
+    execution.PromptQueue(server_instance)
+
+    init_custom_nodes()
+'''
+
+
+from utils.nodes import (
+    KSampler,
+    CheckpointLoaderSimple,
+    EmptyLatentImage,
+    SaveImage,
+    CLIPTextEncode,
+    VAEDecode,
+)
+
+from utils.layerdiffuse import NODE_CLASS_MAPPINGS
+
+with torch.inference_mode():
+    checkpointloadersimple = CheckpointLoaderSimple()
+    checkpointloadersimple = checkpointloadersimple.load_checkpoint(
+        ckpt_name="./models/model.safetensors"
+    )
+
+    def pipe(prompt, negative_prompt, num_outputs, height, width, steps, cfg, sampler_name, scheduler):
+        emptylatentimage = EmptyLatentImage().generate(
+            width=width, height=height, batch_size=num_outputs
+        )
+
+        cliptextencode = CLIPTextEncode()
+
+        cliptextencode_positive = cliptextencode.encode(
+            text=prompt,
+            clip=get_value_at_index(checkpointloadersimple, 1),
+        )
+
+        cliptextencode_negative = cliptextencode.encode(
+            text=negative_prompt, clip=get_value_at_index(checkpointloadersimple, 1)
+        )
+
+        """
+        layereddiffusionapply = NODE_CLASS_MAPPINGS["LayeredDiffusionApply"]().apply_layered_diffusion(
+            config="SDXL, Conv Injection",
+            weight=1,
+            model=get_value_at_index(checkpointloadersimple, 0),
+        )
+        """
+        
+        layereddiffusionapply = NODE_CLASS_MAPPINGS["LayeredDiffusionApply"]().apply_layered_diffusion(
+            config="SDXL, Attention Injection",
+            weight=1,
+            model=get_value_at_index(checkpointloadersimple, 0),
+        )
+
+        ksampler = KSampler().sample(
+            seed=random.randint(1, 2**64),
+            steps=steps,
+            cfg=cfg,
+            sampler_name=sampler_name,
+            scheduler=scheduler,
+            denoise=1.0,
+            model=get_value_at_index(layereddiffusionapply, 0),
+            positive=get_value_at_index(cliptextencode_positive, 0),
+            negative=get_value_at_index(cliptextencode_negative, 0),
+            latent_image=get_value_at_index(emptylatentimage, 0),
+        )
+
+        vaedecode = VAEDecode().decode(
+            samples=get_value_at_index(ksampler, 0),
+            vae=get_value_at_index(checkpointloadersimple, 2),
+        )
+
+        layereddiffusiondecodergba = NODE_CLASS_MAPPINGS["LayeredDiffusionDecodeRGBA"]().decode(
+            sd_version="SDXL",
+            sub_batch_size=16,
+            samples=get_value_at_index(ksampler, 0),
+            images=get_value_at_index(vaedecode, 0),
+        )
+
+        saveimage = SaveImage().save_images(
+            filename_prefix="output",
+            images=get_value_at_index(layereddiffusiondecodergba, 0),
+        )
+
+        return saveimage
+
+class Predictor(BasePredictor):
+    @torch.inference_mode()
+    def predict(
+        self,
+        prompt: str = Input(
+            description="Input prompt",
+            default="abstract beauty, centered, looking at the camera, approaching perfection, dynamic, moonlight, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by Carne Griffiths and Wadim Kashin"
+        ),
+        negative_prompt: str = Input(
+            description="Negative Input prompt",
+            default="watermark, text"
+        ),
+        width: int = Input(
+            description="Width of output image",
+            default=1024
+        ),
+        height: int = Input(
+            description="Height of output image",
+            default=1024
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output.",
+            ge=1,
+            le=4,
+            default=1,
+        ),
+        cfg: float = Input(
+            description="Guidance strength",
+            ge=0.00,
+            le=100.00,
+            default=7.50,
+        ),
+        sampler_name: str = Input(
+            description="sampler",
+            choices=utils.samplers.KSampler.SAMPLERS,
+            default="dpmpp_sde",
+        ),
+        scheduler: str = Input(
+            description="sampler",
+            choices=utils.samplers.KSampler.SCHEDULERS,
+            default="ddim_uniform",
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps", ge=1, le=500, default=25
+        ),
+    ) -> List[Path]:
+        images = pipe(prompt, negative_prompt, num_outputs, height, width, num_inference_steps, cfg, sampler_name, scheduler)['ui']['images']
+
+        output_paths = []
+
+        for i, image in enumerate(images):
+            output_paths.append(Path(f"./utils/output/{image['filename']}"))
+
+        print(output_paths)
+
+        return output_paths
---
+++ utils/nodes.py
@@ -0,0 +1,170 @@
+import os
+import json
+import torch
+from . import latent_preview
+from . import model_management
+from . import folder_paths
+from . import sample
+from . import samplers
+from . import sd
+from .cli_args import args
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+import numpy as np
+from . import utils
+
+class CLIPTextEncode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"text": ("STRING", {"multiline": True}), "clip": ("CLIP", )}}
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "encode"
+
+    CATEGORY = "conditioning"
+
+    def encode(self, clip, text):
+        tokens = clip.tokenize(text)
+        cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
+        return ([[cond, {"pooled_output": pooled}]], )
+
+class VAEDecode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "samples": ("LATENT", ), "vae": ("VAE", )}}
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "decode"
+
+    CATEGORY = "latent"
+
+    def decode(self, vae, samples):
+        return (vae.decode(samples["samples"]), )
+
+def common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent, denoise=1.0, disable_noise=False, start_step=None, last_step=None, force_full_denoise=False):
+    latent_image = latent["samples"]
+    if disable_noise:
+        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
+    else:
+        batch_inds = latent["batch_index"] if "batch_index" in latent else None
+        noise = sample.prepare_noise(latent_image, seed, batch_inds)
+
+    noise_mask = None
+    if "noise_mask" in latent:
+        noise_mask = latent["noise_mask"]
+
+    callback = latent_preview.prepare_callback(model, steps)
+    disable_pbar = not utils.PROGRESS_BAR_ENABLED
+    samples = sample.sample(model, noise, steps, cfg, sampler_name, scheduler, positive, negative, latent_image,
+                                  denoise=denoise, disable_noise=disable_noise, start_step=start_step, last_step=last_step,
+                                  force_full_denoise=force_full_denoise, noise_mask=noise_mask, callback=callback, disable_pbar=disable_pbar, seed=seed)
+    out = latent.copy()
+    out["samples"] = samples
+    return (out, )
+
+class KSampler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                    {"model": ("MODEL",),
+                    "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                    "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                    "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01}),
+                    "sampler_name": (samplers.KSampler.SAMPLERS, ),
+                    "scheduler": (samplers.KSampler.SCHEDULERS, ),
+                    "positive": ("CONDITIONING", ),
+                    "negative": ("CONDITIONING", ),
+                    "latent_image": ("LATENT", ),
+                    "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+                     }
+                }
+
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "sample"
+
+    CATEGORY = "sampling"
+
+    def sample(self, model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=1.0):
+        return common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise)
+    
+class CheckpointLoaderSimple:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "ckpt_name": "string" }}
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
+    FUNCTION = "load_checkpoint"
+
+    CATEGORY = "loaders"
+
+    def load_checkpoint(self, ckpt_name, output_vae=True, output_clip=True):
+        ckpt_path = ckpt_name
+        out = sd.load_checkpoint_guess_config(ckpt_path, output_vae=True, output_clip=True)
+        return out[:3]
+    
+MAX_RESOLUTION=8192
+    
+class EmptyLatentImage:
+    def __init__(self):
+        self.device = model_management.intermediate_device()
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "width": ("INT", {"default": 512, "min": 16, "max": MAX_RESOLUTION, "step": 8}),
+                              "height": ("INT", {"default": 512, "min": 16, "max": MAX_RESOLUTION, "step": 8}),
+                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096})}}
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "generate"
+
+    CATEGORY = "latent"
+
+    def generate(self, width, height, batch_size=1):
+        latent = torch.zeros([batch_size, 4, height // 8, width // 8], device=self.device)
+        return ({"samples":latent}, )
+    
+class SaveImage:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                     "filename_prefix": ("STRING", {"default": "ComfyUI"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+                }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "image"
+
+    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
